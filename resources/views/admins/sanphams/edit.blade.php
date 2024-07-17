98cc2fe1d3626bf09ea62ecb549e69641fc9be80
NguyenCuong-85 <cuongnmph38402@fpt.edu.vn>
2024-07-17 09:47:16 +0700

    haha

--- resources/views/admins/sanphams/edit.blade.php
+++ resources/views/admins/sanphams/edit.blade.php
@@ -7,7 +7,8 @@
         <!-- Page Heading -->
         <h1 class="h3 mb-2 text-gray-800">Thêm Sản Phẩm</h1>
 
-        <!-- DataTales Example -->
+        <!-- DataTales Example kaka -->
+        {{-- Thêm sản phẩm  --}}
         <div class="card shadow mb-4">
             <div class="card-header py-3">
                 <h6 class="m-0 font-weight-bold text-primary">Product</h6>
