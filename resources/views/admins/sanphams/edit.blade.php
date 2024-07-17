d7826cfb4b7548ebabb584919942c2f89b8ab98a
NguyenCuong-85 <cuongnmph38402@fpt.edu.vn>
2024-07-17 09:52:00 +0700

    kkk

--- resources/views/admins/sanphams/edit.blade.php
+++ resources/views/admins/sanphams/edit.blade.php
@@ -7,7 +7,7 @@
         <!-- Page Heading -->
         <h1 class="h3 mb-2 text-gray-800">Thêm Sản Phẩm</h1>
 
-        <!-- DataTales Example kaka -->
+        <!-- DataTales Example kakakaka -->
         {{-- Thêm sản phẩm  --}}
         <div class="card shadow mb-4">
             <div class="card-header py-3">
